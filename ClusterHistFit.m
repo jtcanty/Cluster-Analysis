8f37f0c2b22d3ab010c2929c65d08e3705ffce3e
John Canty <jtcanty117@berkeley.edu>
2015-08-17 19:15:47 -0700

    ClusterHistFit.m

---
+++ ClusterHistFit.m
@@ -0,0 +1,11 @@
+% http://stackoverflow.com/questions/24443370/
+
+% Takes an input of coordinates and bins then into a 2D grid. A 2D
+% histogram is plotted and fit to an elliptical gaussian function.
+% Created by John Canty 08/17/2015
+
+% INPUT: saved_list = list of drift corrected coordinates from
+% Telomere_Cluster_2
+
+% Normalize coordinates and center grid around highest k-value point.
+
